469524f62e0220f220a6f5c5b144d35ca866cf22
Sumit <sumit555dongare@gmail.com>
2024-07-18 23:18:46 +0530

    creating table in SubCategoriesList and adding data from subCategorySlice

--- src/pages/categories/sub-categories/SubCategoriesList.js
+++ src/pages/categories/sub-categories/SubCategoriesList.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import moment from 'moment';
 
 export default function SubCategoriesList() {
+  const subCategories = useSelector((store) => store.subCategories);
   return (
     <div>
-       Sub Categories List
+       <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Category</TableCell>
+              <TableCell >Main Category</TableCell>
+              <TableCell >Total Items</TableCell>
+              <TableCell >Created At</TableCell>
+              <TableCell >Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {subCategories.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell >{row.categoryId}</TableCell>
+                <TableCell >{row.totalItems}</TableCell>
+                <TableCell >{ moment(row.createdAt).format('DD MMM YYYY')}</TableCell>
+                <TableCell ><div>Actions</div></TableCell>
+                
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   )
 }
