d40503fcfdd58422fb7980108a864d0c6874a298
Sumit <sumit555dongare@gmail.com>
2024-07-25 12:35:19 +0530

    creating SELECT options by fetching data from mainCategories in CreateSubCategory

--- src/pages/categories/sub-categories/CreateSubCategory.js
+++ src/pages/categories/sub-categories/CreateSubCategory.js
@@ -1,9 +1,140 @@
-import React from 'react'
+import React from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { Button, MenuItem, Paper, TextField } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { Description } from "@mui/icons-material";
+import axios from "axios";
+import { API_BASE_URL } from "../../../utils/ApiConstants";
+import { useSelector } from "react-redux";
 
 export default function CreateSubCategory() {
+  const navigate = useNavigate();
+  const mainCategories = useSelector((store) => store.mainCategories);
+
+  const validationSchema = yup.object({
+    name: yup.string("Enter your email").required("Name is required"),
+    description: yup
+      .string("Enter description")
+      .min(3, "Description should be of minimum 3 characters length")
+      .required("Description is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      description: "",
+    },
+
+    validationSchema: validationSchema,
+
+    onSubmit: (values) => {
+      // API call
+      console.log(values)
+      // axios
+      //   .post(`${API_BASE_URL}/categories`, {
+      //     id: Math.floor(Math.random() * (100 - 10 + 1)) + 10, //Generate random id from 10-100
+      //     name: values.name,
+      //     description: values.description,
+      //   })
+      //   .then(function (response) {
+      //     // handle success
+      //     // console.log("Categories Response",response.data);
+
+      //     // const data = response.data;
+      //     alert("Category created!");
+      //     // getCategories();
+      //     navigate(-1);
+      //   })
+      //   .catch(function (error) {
+      //     // handle error
+      //     alert("Failed to create category");
+      //     console.log("There is an error", error);
+      //   })
+      //   .finally(function () {
+      //     // always executed
+      //   });
+    },
+  });
+
   return (
-    <div>
-       CreateSubCategory
+    <div className="create-main-category-component ">
+      <Paper elevation={3} className="form-container p-3">
+        <form
+          className="d-flex align-items-center flex-column"
+          onSubmit={formik.handleSubmit}
+        >
+          <h3>Add Sub Category</h3>
+          <TextField
+            name="name"
+            fullWidth
+            label="Category Name"
+            className="mb-3"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.email && Boolean(formik.errors.name)}
+            helperText={formik.touched.email && formik.errors.name}
+          />
+
+          <TextField
+            multiline
+            rows={4}
+            name="description"
+            fullWidth
+            label="Description"
+            className="mb-3"
+            value={formik.values.description}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={
+              formik.touched.description && Boolean(formik.errors.description)
+            }
+            helperText={formik.touched.description && formik.errors.description}
+          />
+
+          <TextField
+           
+            select
+            name="categoryId"
+            label="Select"
+            fullWidth
+            defaultValue=""
+            helperText="Please select your currency"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={
+              formik.touched.categoryId && Boolean(formik.errors.categoryId)
+            }
+            helperText = {formik.touched.categoryId && formik.errors.categoryId}
+
+          >  
+             <MenuItem  value=" " disabled> Select Category </MenuItem>
+              {mainCategories.map(cat =>{
+                return <MenuItem  value={cat.id}>
+
+                {cat.name}
+              </MenuItem>
+              })}
+
+            
+          </TextField>
+
+
+
+          <Button variant="contained" type="submit" className="mt-5">
+            Create Category
+          </Button>
+          <Button
+            onClick={() => {
+              navigate(-1); //is used for go to back url
+            }}
+          >
+            {" "}
+            Cancel
+          </Button>
+        </form>
+      </Paper>
     </div>
-  )
+  );
 }
--- src/pages/categories/sub-categories/SubCategoriesList.js
+++ src/pages/categories/sub-categories/SubCategoriesList.js
@@ -1,15 +1,17 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import moment from 'moment';
 import axios from 'axios';
 import { setSubCategories } from '../../../store/subCategorySlice';
 import { API_BASE_URL } from '../../../utils/ApiConstants';
+import { useNavigate } from 'react-router-dom';
 
 export default function SubCategoriesList() {
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const mainCategories = useSelector((store) => store.mainCategories);
 
   const mapping = mainCategories.reduce((mapp, category)=>{
@@ -53,6 +55,10 @@ export default function SubCategoriesList() {
 
   return (
     <div>
+      <Button  variant="contained" onClick={()=>{
+        navigate('create')
+      }}>Add Sub Category</Button>
+      
        <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
