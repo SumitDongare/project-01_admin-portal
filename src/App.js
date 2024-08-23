2f2302147d386ef7e8ac658dc05b5bfe27800b17
Sumit <sumit555dongare@gmail.com>
2024-08-23 12:31:29 +0530

    how to protect the route

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-
-import { Navigate, Route, Routes } from 'react-router-dom';
-import './App.scss';
-import LoginComponent from './auth/LoginComponent';
-import { lazy, Suspense } from 'react';
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import LoginComponent from "./auth/LoginComponent";
+import { lazy, Suspense } from "react";
+import ProtectedRoute from "./components/ProtectedRoute";
 // import PagesComponent from './pages/PagesComponent';
-const PagesComponent = lazy(()=> import('./pages/PagesComponent'));
-
+const PagesComponent = lazy(() => import("./pages/PagesComponent"));
 
 function App() {
   return (
-  <div className='app-component' >
-    <Routes>
-      <Route path='/' element={<Navigate to="/login" replace={true} />}  ></Route>
-       <Route path='login' element={<LoginComponent></LoginComponent>}></Route>
-       <Route path='pages/*' element={
-        <Suspense fallback ={<div>Loadding.....</div>}>
-          <PagesComponent></PagesComponent>
-        </Suspense>}></Route>
-
-    </Routes>
-        
-       
-       
-  </div>
+    <div className="app-component">
+      <Routes>
+        <Route
+          path="/"
+          element={<Navigate to="/login" replace={true} />}
+        ></Route>
+        <Route path="login" element={<LoginComponent></LoginComponent>}></Route>
+        <Route
+          path="pages/*"
+          element={
+            <Suspense fallback={<div>Loadding.....</div>}>
+              <ProtectedRoute>
+                <PagesComponent></PagesComponent>
+              </ProtectedRoute>
+            </Suspense>
+          }
+        ></Route>
+      </Routes>
+    </div>
   );
 }
 
